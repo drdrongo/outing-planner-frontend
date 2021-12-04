caa3d6e5d5944c3b4bea7d32f1d7cb7b6a3b8315
hayato clarke <hayatoclarke@gmail.com>
2021-12-04 12:46:06 +0900

    react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Outlet, Link } from "react-router-dom";
+
+import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import ThreeDRotation from '@mui/icons-material/ThreeDRotation';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <nav style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+        }}>
+          <Link to="/invoices">Invoices</Link> |{" "}
+          <Link to="/expenses">Expenses</Link>
+        </nav>
+        <Outlet />
+
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <AccessAlarmIcon/>
+        <ThreeDRotation/>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Resact
         </a>
       </header>
     </div>
